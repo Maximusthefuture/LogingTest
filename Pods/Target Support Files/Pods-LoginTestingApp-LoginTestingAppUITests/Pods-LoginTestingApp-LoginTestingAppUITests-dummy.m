49273f446cab291c5bdb523f41a9e852b3bee1c5
MaximusTheFuture <naturemade@yandex.ru>
2022-02-15 22:03:16 +0300

    deleted pods

--- Pods/Target Support Files/Pods-LoginTestingApp-LoginTestingAppUITests/Pods-LoginTestingApp-LoginTestingAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LoginTestingApp_LoginTestingAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LoginTestingApp_LoginTestingAppUITests
-@end
--- Pods/Target Support Files/Pods-LoginTestingApp/Pods-LoginTestingApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LoginTestingApp : NSObject
-@end
-@implementation PodsDummy_Pods_LoginTestingApp
-@end
